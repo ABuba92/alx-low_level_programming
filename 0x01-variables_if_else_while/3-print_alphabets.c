05ef05efef3874550fb0d26cdb4db8e4888434c1
AHMAD ABUBAKAR UMAR <ahmadabubakarumar1992@yahoo.com>
2022-09-12 12:46:55 -0700

    3file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - starting point of the program
+ *
+ * Description: displaying lower and uppercase letters
+ *
+ * Return: returning value
+ */
+int main(void)
+{
+char n = 'a'
+char m = 'A'
+for(n <= 'z')
+{
+putchar(n);
+n++;
+}
+for(m <= 'Z')
+{
+putchar(m);
+m++;
+}
+putchar('\n');
+return (0);
+}
