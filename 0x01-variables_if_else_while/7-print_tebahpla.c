78dcbd273eea80ec37fb38ad3489264c28ab2c8e
AHMAD ABUBAKAR UMAR <ahmadabubakarumar1992@yahoo.com>
2022-09-12 17:06:11 -0700

    7file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Description: my description in reverse
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+int i;
+for (i = 'z'; i >='a'; i--)
+{
+	putchar(i);
+}
+putchar("\n");
+return (0);
+}
