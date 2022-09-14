261e99bb1624c93aa83e9ac6b897ace922917fb4
AHMAD ABUBAKAR UMAR <ahmadabubakarumar1992@yahoo.com>
2022-09-14 15:31:25 -0700

    myfile

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks alphabet
+ *
+ * @c: c is c
+ *
+ * Return: 1 or 0
+ */
+int _isalpha(int c)
+{
+	for ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
