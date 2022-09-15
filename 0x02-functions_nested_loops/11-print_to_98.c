59efde6da2e51968d1702e36bfe961ebe80f2bf7
AHMAD ABUBAKAR UMAR <ahmadabubakarumar1992@yahoo.com>
2022-09-14 21:22:13 -0700

    bauer

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints
+ *
+ * @n: starting
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	int lim = 98;
+
+	if (n < lim)
+	{
+		while (n <= lim)
+		{
+			printf("%d", n);
+			if (n != lim)
+			{
+				printf(", ");
+			}
+			else if (n == lim)
+			{
+				printf("\n");
+			}
+			n++;
+		}
+	}
+}
+else if (n > lim)
+{
+	while (n >= lim)
+	{
+		printf("%d", n);
+		if (n != lim)
+		{
+			printf(", ");
+		}
+		else if (n == lim)
+		{
+			printf("\n");
+		}
+		n--;
+	}
+}
+else
+{
+	printf("98\n");
+}
