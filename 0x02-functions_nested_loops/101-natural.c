37afadfc8615f4c585609513070c4c1c6612bedc
AHMAD ABUBAKAR UMAR <ahmadabubakarumar1992@yahoo.com>
2022-09-15 12:29:14 -0700

    late

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - main function
+ * @void - no args
+ * Return: void
+ */
+
+int main(void)
+{
+	int sum = 0;
+	int i = 0;
+
+	for (i = 0; i < 1024; i++)
+		if (i % 3 == 0 || i % 5 == 0)
+			sum = sum + i;
+
+	printf("%d\n", sum);
+	return (0);
+}
