24859ddfed82bb814998b6f98c27b9916aba4407
AHMAD ABUBAKAR UMAR <ahmadabubakarumar1992@yahoo.com>
2022-09-18 16:37:57 -0700

    numbers

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,6 +3,7 @@
 /**
  * print_diagonal - printing line diagonally
  * @n: character to be used
+ *
  * Return: void
  */
 
@@ -11,16 +12,14 @@ void print_diagonal(int n)
 	int i, j;
 
 	if (n <= 0)
-	{
 		_putchar('\n');
-	}
 	else
 	{
 		for (i = 1; i <= n; i++)
 		{
 			for (j = 1; j < i; j++)
 				_putchar(' ');
-			_putchar(98);
+			_putchar(92);
 			_putchar('\n');
 		}
 	}
