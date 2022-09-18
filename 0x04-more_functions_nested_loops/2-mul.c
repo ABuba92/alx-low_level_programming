5e6cfb45fba5bfe71baaa0f4981820c42f62894f
AHMAD ABUBAKAR UMAR <ahmadabubakarumar1992@yahoo.com>
2022-09-18 12:17:05 -0700

    mult

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ * @a: integer one
+ * @b: integer two
+ * Return: zero
+ */
+
+int mul(int a, int b)
+{
+	int result;
+	result = a * b;
+}
