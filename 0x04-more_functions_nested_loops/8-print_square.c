f7761b124e06778af6bd3d93b184891eee3802f2
AHMAD ABUBAKAR UMAR <ahmadabubakarumar1992@yahoo.com>
2022-09-18 17:08:09 -0700

    numbers

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,14 +11,15 @@ void print_square(int size)
 	int i, j;
 
 	if (size <= 0)
-		_putchar('\n');
-	else
 	{
-		for (i = 1; i <= size; i++)
-		{
-			for (j = 1; j <= size; j++)
-				_putchar('#');
-		}
+		_putchar('\n');
+		goto END;
+	}
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+			_putchar('#');
 		_putchar('\n');
 	}
+END:;
 }
