2bc1477cc4bb11f0520d481305857d583169a5c6
AHMAD ABUBAKAR UMAR <ahmadabubakarumar1992@yahoo.com>
2022-09-20 16:04:32 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a given string
+ * @s: string parameter
+ * Return: o
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*(s + length) != '\0')
+		length++;
+
+	return (0);
+}
