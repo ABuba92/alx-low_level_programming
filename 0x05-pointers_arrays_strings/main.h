a12a9312c1858f6e21d0b5a61c562649244f52d9
AHMAD ABUBAKAR UMAR <ahmadabubakarumar1992@yahoo.com>
2022-09-20 17:37:52 -0700

    pointers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifdef MAIN_H
 #define MAIN_H
-#include <stdlib.hi>
+#include <stdlib.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -13,6 +13,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *str);
+int _atoi(char *s);
 
 #endif
