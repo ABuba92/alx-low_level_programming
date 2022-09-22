88f4e1bf0507ccca8c88f6b4590ebd31ae1e51a2
AHMAD ABUBAKAR UMAR <ahmadabubakarumar1992@yahoo.com>
2022-09-22 16:30:38 -0700

    more pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,16 +13,16 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int destLength = 0;
 	int counter = 0;
-	
+
 	while (dest[counter] != '\0')
 	{
 		destLength++;
 		counter++;
 	}
-	
+
 	for (i = 0; src[i] != '\0'; i++)
 		dest[destLength + i] = src[i];
 	dest[destLength + i] = '\0';
-	
+
 	return (dest);
 }
