e67490a5fd9ed5c807998dfabac2077206d00000
AHMAD ABUBAKAR UMAR <ahmadabubakarumar1992@yahoo.com>
2022-09-22 17:19:26 -0700

    more pointers

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,7 @@
 int _strcmp(char *s1, char *s2)
 {
 	int count = 0, count2 = 0;
-	
+
 	while (count == 0)
 	{
 		if (*(s1 + count2) == '\0' && *(s2 + count2) == '\0')
@@ -19,6 +19,6 @@ int _strcmp(char *s1, char *s2)
 		count = *(s1 + count2) - *(s2 + count2);
 		count2++;
 	}
-	
+
 	return (count);
 }
